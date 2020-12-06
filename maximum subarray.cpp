fc045f2758a0e511ec6fdd96cc71eda604d41772
sunanda690 <58281090+sunanda690@users.noreply.github.com>
2020-12-06 18:13:43 +0530

    Add files via upload

---
+++ maximum subarray.cpp
@@ -0,0 +1,21 @@
+//O(n) solution using DP
+class Solution
+{
+public:
+	int maxSubArray(vector<int>& nums){
+		if (nums.size() == 1)
+		{
+			return nums[0];
+		}
+		int maximum_found = nums[0];
+		int current_sum = nums[0];
+		for (int i = 1; i < nums.size(); ++i)
+		{
+			current_sum = max(nums[i], current_sum+nums[i]);
+			maximum_found = max(maximum_found,current_sum);
+		}
+		return maximum_found;
+	}
+	
+};
+
