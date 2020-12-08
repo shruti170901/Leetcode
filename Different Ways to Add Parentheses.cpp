363f26c906fde2e66baa1305933b0931c0436157
N Balgopal Patro <46865367+BalgopalPatro@users.noreply.github.com>
2020-12-08 18:26:07 +0530

    Update Different Ways to Add Parentheses.cpp

--- Different Ways to Add Parentheses.cpp
+++ Different Ways to Add Parentheses.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     vector<int> diffWaysToCompute(string input) {
         vector<int> ans;
-        bool flag = true;
+        bool f = true;
         int size = input.length();
             for(size_t i=0;i<size;i++){
-                char ch = input[i];
-                if(ch == '+' || ch == '-' || ch=='*'){
-                    flag = false;
-                    vector<int> ans_left = diffWaysToCompute(input.substr(0,i));
+                char chr = input[i];
+                if(chr == '+' || chr == '-' || chr=='*'){
+                    f = false;
+                    vector<int> left_ans = diffWaysToCompute(input.substr(0,i));
                     vector<int> ans_right = diffWaysToCompute(input.substr(i+1));
                     
-                    for(int n =0; n < ans_left.size();n++){
+                    for(int n =0; n < left_ans.size();n++){
                         for(int m =0; m< ans_right.size();m++){
-                            switch(ch){
+                            switch(chr){
                                 case '+':
-                                    ans.push_back(ans_left[n] + ans_right[m]);
+                                    ans.push_back(left_ans[n] + ans_right[m]);
                                         break;
                                     case '-':
-                                    ans.push_back(ans_left[n] - ans_right[m]);
+                                    ans.push_back(left_ans[n] - ans_right[m]);
                                         break;
                                     case '*':
-                                    ans.push_back(ans_left[n] * ans_right[m]);
+                                    ans.push_back(left_ans[n] * ans_right[m]);
                                         break;
                             }
                         }
@@ -29,7 +29,7 @@ public:
                     
                 }
             }
-        if(flag){
+        if(f){
             ans.push_back(stoi(input));
             return ans;
         }
