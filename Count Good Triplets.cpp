893384c2b8415f60237c7dcdcd8bba236241091d
Nitish Kumar <nitish705702@gmail.com>
2020-12-09 22:05:32 +0530

    Create Count Good Triplets.cpp

---
+++ Count Good Triplets.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int cnt=0;
+        for(int i=0;i<arr.size()-2;i++){
+            for(int j=i+1;j<arr.size()-1;j++){
+                for(int k=j+1;k<arr.size();k++){
+                    if((abs(arr[i]-arr[j])<=a) && (abs(arr[j]-arr[k])<=b) && (abs(arr[k]-arr[i])<=c))cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
