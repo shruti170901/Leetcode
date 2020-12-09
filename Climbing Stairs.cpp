80acc0cd1694e804bddfb582f486873144a316a8
Nitish Kumar <nitish705702@gmail.com>
2020-12-09 21:49:07 +0530

    Create Climbing Stairs.cpp

---
+++ Climbing Stairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int fib(int n) { 
+        double phi = (1 + sqrt(5)) / 2; 
+        return round(pow(phi, n) / sqrt(5)); 
+    } 
+    int climbStairs(int n) {
+         n=fib(n+1);
+        return n;
+    }
+};
