f36992b1809fa1b16fb3a1c3b5b2f9ef442dd551
Jaisai <jaisaikrishnan6@gmail.com>
2020-12-12 21:57:48 +0530

    Added solution Kth Largest Element in an Array

--- the kth factor of n.cpp
+++ the kth factor of n.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
-    int kthFactor(int n, int k) 
-    {  
-        vector<int> v;
-        int count=0;
-        for (int i=1; i<=sqrt(n); i++) 
-        { 
-            if (n%i == 0) 
-            { 
-                count++;
-                if (n/i == i){
-                    v.push_back(i);
-                }
-
-                else {
-                    v.push_back(i);
-                    v.push_back(n/i);
-                    //printf("%d %d ", i, n/i);
-                }
-            } 
+    int kthFactor(int n, int k) {
+        int cnt;
+        for(int i=1;i<=n;i++){
+            if(k>0 && n%i==0){
+                cnt=i;
+                k--;
+            }
         }
-        sort(v.begin(),v.end());
-        if(k<=v.size())return v[k-1];
-        else return -1;
-    } 
+        if(k!=0)cnt=-1;
+        return cnt;
+    }
 };
