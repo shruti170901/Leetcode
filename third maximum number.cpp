05793ece370e5a2bebf5b1a023f9f8dd54f7ee44
vikhyat singh <vikhyatsingh628@gmail.com>
2020-12-13 02:25:55 +0530

    Update changes

--- third maximum number.cpp
+++ third maximum number.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        int x;
-        sort(nums.begin(),nums.end());
-        set<int> s(nums.begin(), nums.end());
-        if(s.size() >= 3)
-            x = *std::next(s.begin(), s.size()-3);
-        else
-            x = *std::next(s.begin(), s.size()-1);
-        return x;
-    }
+       vector<int> a(4, 0);
+        vector<bool> b(4, false);
 
+        for (auto x : nums) {
+            if (b[1] && x == a[1]) continue;
+            if (b[2] && x == a[2]) continue;
+            if (b[3] && x == a[3]) continue;
+            
+            b[0] = true;
+            a[0] = x; 
+            
+            for(int i = 1; i < 4; ++i) {
+                if (b[i] && a[i] > x) break;
+                swap(a[i], a[i-1]);
+                swap(b[i], b[i-1]);
+            }
+        }
+    
+        if(b[1])
+            return a[1];
+        else
+            return a.back();
+        
+    }
 };
