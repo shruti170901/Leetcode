76dd5baf3c1372aea972605ebc26ccc601930e48
shruti170901 <58871601+shruti170901@users.noreply.github.com>
2020-12-15 18:58:42 +0530

    Revert "added solutions to 3 questions"

--- code4.c
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int maxDepth(TreeNode* root) {
-        if (!root) {
-            return 0;
-        }
-        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
-    }
-};
