a9aa7346b5fef161ca2b48d234abde7e7ada6dd9
Piyush Kumar <50403579+Panda6388@users.noreply.github.com>
2020-12-16 11:32:21 +0530

    #4 problems related to binary search

---
+++ Magnetic force between two balls.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/contest/weekly-contest-202/problems/magnetic-force-between-two-balls/
+// Time: O(log(max(A)) * N + NlogN)
+// Space: O(1)
+class Solution {
+    bool valid(vector<int> &A, int M, int m) {
+        int prev = 0;
+        for (int i = 1, j = 1; i < m; ++i) {
+            while (j < A.size() && A[j] < A[prev] + M) ++j;
+            if (j >= A.size()) return false;
+            prev = j;
+        }
+        return true;
+    }
+public:
+    int maxDistance(vector<int>& A, int m) {
+        sort(begin(A), end(A));
+        if (m == 2) return A.back() - A[0];
+        int L = 1, R = A.back() - A[0];
+        while (L <= R) {
+            int M = (L + R) / 2;
+            if (valid(A, M, m)) L = M + 1;
+            else R = M - 1;
+        }
+        return R;
+    }
+};
