9acf0004e4db5f1c0c1aa364a968a123eb545f23
Aarushi11H <67260600+Aarushi11H@users.noreply.github.com>
2020-12-22 18:54:34 +0530

    Stone game VI

---
+++ Stone Game VI.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int stoneGameVI(vector<int>& aliceValues, vector<int>& bobValues) {
+        vector<pair<int, int>> stones;
+        
+        for (int i = 0; i < aliceValues.size(); ++i) {
+            stones.push_back({aliceValues[i] + bobValues[i], i});
+        }
+        
+        sort(stones.rbegin(), stones.rend());
+        
+        int a = 0;
+        int b = 0;
+        
+        for (int i = 0; i < stones.size(); ++i) {
+            if (i % 2 == 0) {
+                a += aliceValues[stones[i].second];
+            } else {
+                b += bobValues[stones[i].second];
+            }
+        }
+        
+        return a == b ? 0 : a > b ? 1 : -1;
+    }
+};
