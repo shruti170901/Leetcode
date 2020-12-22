07eb3ec721bd3838bf542c97293c6187d356b54c
anushkasinghal11 <singhalanushka.1999@gmail.com>
2020-12-22 20:45:54 +0530

    reverse vowels of a string

---
+++ Binary Subarrays With Sum.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    int numSubarraysWithSum(vector<int> &A, int K)
+    {
+        return (countAtLeastK(A, K) - countAtLeastK(A, K + 1));
+    }
+    int countAtLeastK(vector<int> &A, int K)
+    {
+        int size = A.size();
+        int answer = 0;
+        int windowSum = 0;
+        for (int left = 0, right = 0; right < size; right++)
+        {
+            windowSum += A[right];
+            while (left <= right && windowSum >= K)
+            {
+                windowSum -= A[left];
+                left++;
+            }
+            answer += left;
+        }
+        return answer;
+    }
+};
