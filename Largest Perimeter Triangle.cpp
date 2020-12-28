49807443ae6edc2ac79abee9119493cf1d42b6f8
anushkasinghal11 <singhalanushka.1999@gmail.com>
2020-12-28 12:30:00 +0530

    largest perimeter triangle

---
+++ Largest Perimeter Triangle.cpp
@@ -0,0 +1,11 @@
+sort(A.begin(), A.end());
+int n = A.size() - 1;
+for (int i = n; i >= 2; i--)
+{
+    if (A[i - 1] + A[i - 2] > A[i])
+    {
+        return A[i] + A[i - 1] + A[i - 2];
+    }
+}
+
+return 0;
